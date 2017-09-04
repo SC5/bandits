56b7865bb5cdb891f24d854368dee69a125ba27d
Max Pagels <max.pagels@sc5.io>
2017-09-04 12:51:24 +0300

    Add support for classification models.

--- app.py
+++ app.py
@@ -9,22 +9,23 @@ np.random.seed(1337)
 
 # Use the hashing trick to make feature vectors of uniform length (n_features) regardless
 # of the number of features passed in the context (remaning features are set to zero)
-vectorizer = HashingVectorizer(n_features=100)
-contextual_bandit = bandit.epsilonGreedyContextualBandit()
+vectorizer = HashingVectorizer(n_features=10)
+contextual_bandit = bandit.epsilonGreedyContextualBandit(mode='classification', epsilon=0.2, penalty='none')
 
 # Example with three advertisments we would like to show
 arms = ['advertisment_1', 'advertisment_2', 'advertisment_3', 'advertisment_4']
 
 # The job of the bandit is to learn the true click-through rates
-# of each arm, but for simulation purposes, we'll cheat and pretend 
+# of each arm, but for simulation purposes, we'll cheat and pretend
 # we already know.
-ctrs = [0.4, 0.052, 0.0522, 0.0521]
+ctrs = [0.056, 0.052, 0.0122, 0.0521]
 
 # Simulate a single context, a male website visitor aged 21 that uses Firefox
 context = vectorizer.fit_transform(['age_21 gender_male browser_firefox'])
 counts = np.zeros(len(arms)) # Keep count of how many times each arm was chosen
+rewards = np.zeros(len(arms)) # Keep count of the rewards for each arm
 
-epochs = 100000
+epochs = 1000
 print('Running simulation for ' + str(epochs) + ' epochs')
 for i in range(epochs):
     sys.stdout.write('.')
@@ -35,10 +36,13 @@ for i in range(epochs):
     # - 0: not clicked
     if random.random() <= ctrs[arms.index(chosen_arm)]:
         contextual_bandit.reward(chosen_arm, context, 1)
+        rewards[arms.index(chosen_arm)] += 1
     else:
         contextual_bandit.reward(chosen_arm, context, 0)
+        rewards[arms.index(chosen_arm)] += 0
     counts[arms.index(chosen_arm)] += 1
 
 print('done.\nResults:')
 for i, v in enumerate(counts):
     print('Arm ' + arms[i] + ' was chosen ' + str(counts[i]) + ' times.')
+    print('Arm ' + arms[i] + ' has a cumulative reward of ' + str(rewards[i]) + '.')
--- bandit.py
+++ bandit.py
@@ -3,14 +3,19 @@ import random
 import numpy as np
 
 from sklearn.exceptions import NotFittedError
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import SGDRegressor, SGDClassifier
+
+class unknownModeException(Exception):
+    pass
 
 class epsilonGreedyContextualBandit(object):
 
-    def __init__(self, epsilon=0.2, fit_intercept=True):
+    def __init__(self, mode='regression', epsilon=0.2, fit_intercept=True, penalty='l2'):
         self.config = {
             'epsilon': epsilon,
-            'fit_intercept': fit_intercept
+            'fit_intercept': fit_intercept,
+            'mode': mode,
+            'penalty': penalty
         }
         self.arms = {}
         self.n_arms = 0
@@ -18,13 +23,26 @@ class epsilonGreedyContextualBandit(object):
     def select_arm(self, context, arms):
         for arm in arms:
             if arm not in self.arms:
-                # Initialise a new linear regression model for predicting the reward
-                self.arms[arm] = SGDRegressor(
-                    fit_intercept=self.config['fit_intercept'],
-                    max_iter=5,
-                    tol=None
-                )
+                # Initialise a new linear regression or classification model for predicting the reward
+                if self.config['mode'] is 'regression':
+                    self.arms[arm] = SGDRegressor(
+                        fit_intercept=self.config['fit_intercept'],
+                        penalty=self.config['penalty'],
+                        max_iter=1,
+                        tol=None
+                    )
+                elif self.config['mode'] is 'classification':
+                    self.arms[arm] = SGDClassifier(
+                        fit_intercept=self.config['fit_intercept'],
+                        penalty=self.config['penalty'],
+                        loss='log',
+                        max_iter=1,
+                        tol=None
+                    )
+                else:
+                    raise unknownModeException("Unknown mode (must be either 'regression' or 'classification' ")
                 self.n_arms += 1
+
         if random.random() <= self.config['epsilon']:
             return random.choice(list(self.arms.keys()))
         else:
@@ -32,12 +50,18 @@ class epsilonGreedyContextualBandit(object):
                 predictions = []
                 candidates = []
                 arms = self.arms.keys()
-                for arm in self.arms.keys():
-                    predictions.append(self.arms[arm].predict(context))
+                for arm in arms:
+                    if self.config['mode'] is 'regression':
+                        predictions.append(self.arms[arm].predict(context))
+                    else:
+                        predictions.append(self.arms[arm].predict_proba(context)[0][1])
                     candidates.append(arm)
                 return candidates[np.argmax(predictions)]
             except NotFittedError:
                 return random.choice(list(self.arms.keys()))
 
     def reward(self, arm, context, reward):
-        self.arms[arm].partial_fit(context, [reward])
+        if self.config['mode'] is 'regression':
+            self.arms[arm].partial_fit(context, [reward])
+        else:
+            self.arms[arm].partial_fit(context, [reward], classes=[0,1])
