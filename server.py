be4dc1a39d6bbb047c52065045f50bbcd3467953
Max Pagels <max.pagels@sc5.io>
2017-09-20 10:04:25 +0300

    Add experimental Sanic code.

---
+++ server.py
@@ -0,0 +1,28 @@
+import bandit
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+contextual_bandit = bandit.epsilonGreedyContextualBandit(epsilon=0.1, penalty='l2')
+
+@app.route("/predict", methods=["POST"])
+async def predict(request):
+    body = request.json
+    context = ' '.join(body['context'])
+    arms = body['arms']
+    arm = contextual_bandit.select_arm(context, arms)
+    return json({"arm": arm})
+
+@app.route("/reward", methods=["POST"])
+async def predict(request):
+    body = request.json
+    context = ' '.join(body['context'])
+    arm = body['arm']
+    cost = body['cost']
+    print(arm, cost)
+    contextual_bandit.reward(arm, context, cost)
+    return json({"cost": cost})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
