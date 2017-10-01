9d4c9cae03d0849f53b64cb986e277f86db73378
Max Pagels <max.pagels@sc5.io>
2017-10-01 17:19:16 +0300

    Remove unused code and unnecessary comment.

--- app.py
+++ app.py
@@ -2,29 +2,24 @@ import sys
 import numpy as np
 import bandit
 
-np.random.seed(1337)
+# np.random.seed(2)
 
-from sklearn.feature_extraction.text import HashingVectorizer
-
-# Use the hashing trick to make feature vectors of uniform length (n_features) regardless
-# of the number of features passed in the context (remaining features are set to zero)
-vectorizer = HashingVectorizer(n_features=1024)
-contextual_bandit = bandit.epsilonGreedyContextualBandit(epsilon=0.1, penalty='l2')
+contextual_bandit = bandit.epsilonGreedyContextualBandit(epsilon=0.1, penalty='l1')
 
 # Example with advertisments we would like to show
-arms = ['advertisement_1', 
-        'advertisement_2', 
-        'advertisement_3', 
-        'advertisement_4', 
-        'advertisement_5', 
-        'advertisement_6', 
-        'advertisement_7', 
+arms = ['advertisement_1',
+        'advertisement_2',
+        'advertisement_3',
+        'advertisement_4',
+        'advertisement_5',
+        'advertisement_6',
+        'advertisement_7',
         'advertisement_8']
 
 # The job of the bandit is to learn the true click-through rates
 # of each arm, but for simulation purposes, we'll cheat and pretend
 # we already know.
-ctrs = [0.076, 0.0521, 0.0122, 0.05215, 0.074, 0.0521, 0.07482, 0.0154]
+ctrs = [0.993, 0.0521, 0.0122, 0.05215, 0.074, 0.0521, 0.07482, 0.0154]
 
 # Simulate a single context, a male website visitor aged 21 that uses Firefox
 context = 'age_21 gender_male browser_firefox'
@@ -32,7 +27,7 @@ context = 'age_21 gender_male browser_firefox'
 counts = np.zeros(len(arms)) # Keep count of how many times each arm was chosen
 costs = np.zeros(len(arms)) # Keep count of the costs for each arm
 
-epochs = 50000
+epochs = 10000
 sys.stdout.write('Running simulation for ' + str(epochs) + ' epochs')
 for i in range(epochs):
     sys.stdout.write('.')
@@ -41,9 +36,9 @@ for i in range(epochs):
     # Send cost based on our pretend CTR for the chosen arm:
     # - -1: clicked
     # - 1: not clicked
-    if np.random.random() <= ctrs[arms.index(chosen_arm)]:
-        contextual_bandit.reward(chosen_arm, context, -1)
-        costs[arms.index(chosen_arm)] += -1
+    if np.random.random() < ctrs[arms.index(chosen_arm)]:
+        contextual_bandit.reward(chosen_arm, context, 0)
+        costs[arms.index(chosen_arm)] += 0
     else:
         contextual_bandit.reward(chosen_arm, context, 1)
         costs[arms.index(chosen_arm)] += 1
--- bandit.py
+++ bandit.py
@@ -1,4 +1,3 @@
-# Imports
 import numpy as np
 
 from sklearn.exceptions import NotFittedError
